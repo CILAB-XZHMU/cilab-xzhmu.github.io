04b3cc94b24454d0de0b93ce8cea160c708833a0
CILAB-XZHMU <CILAB_XZHMU@outlook.com>
2025-01-04 14:51:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+window.onscroll = function() {
+    var button = document.getElementById("backToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
